7436316c71b46494c207f1b41abc522e7709ede4
Cong Dinh <congdinh2021@gmail.com>
2021-08-05 11:35:04 +0700

    Create basic project

---
+++ src/FA.IdentitySample/FA.IdentitySample.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FA.IdentitySample.WebMVC.Startup))]
+namespace FA.IdentitySample.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
